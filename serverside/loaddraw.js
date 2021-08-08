f9ede69e8f9637380583bf27e44551153cb02dc8
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2021-08-08 11:58:25 -0400

    Update loaddraw.js

--- serverside/loaddraw.js
+++ serverside/loaddraw.js
@@ -84,8 +84,8 @@ body{
 
             </div>
         </div>
-        <div style="width:300px;height:220px background-color: #ffffff;">
-        <canvas id="drawing" width="270px", height="220px">
+        <div style="width:300px;height:220px;">
+        <canvas id="drawing" width="270px", height="220px" style="background-color: #ffffff;">
                 Sorry, your browser doesn't support canvas technology.
             </canvas>
 
